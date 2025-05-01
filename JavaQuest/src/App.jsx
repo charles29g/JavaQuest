e5f3457c03d64fc649ff773cad91707760b6c901
BagBags <aaronbagain@gmail.com>
2025-05-01 22:17:47 +0800

    Yes
    dasdd

--- JavaQuest/src/App.jsx
+++ JavaQuest/src/App.jsx
@@ -5,18 +5,23 @@ import "./Design.css";
 import LandingPage from "./Components/userComponent/landingPage";
 import ModulePage from "./Components/userComponent/modulePage";
 import ModuleLessons from "./Components/userComponent/moduleLessons.jsx";
+import QuizPageInstructions from "./Components/userComponent/quizPageInstructions.jsx";
 
 import { ModuleItems, ModuleContents, KCQA } from "./Components/data.js";
 
 export default function App() {
   const [moduleID, setModuleID] = useState(1);
 
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/modules" element={<ModulePage ModuleItems={ModuleItems} setModuleID={setModuleID} />} />
+        <Route
+          path="/modules"
+          element={
+            <ModulePage ModuleItems={ModuleItems} setModuleID={setModuleID} />
+          }
+        />
         <Route
           path="/lessons"
           element={
@@ -24,10 +29,19 @@ export default function App() {
               ModuleContents={ModuleContents}
               moduleID={moduleID}
               KCQA={KCQA}
-             
             />
           }
         />
+        <Route
+          path="/quiz"
+          element={
+            <QuizPageInstructions
+              moduleID={moduleID}
+              ModuleItems={ModuleItems}
+            />
+          }
+        />
+
         <Route path="*" element={<h2>Page Not Found</h2>} />
       </Routes>
     </Router>
