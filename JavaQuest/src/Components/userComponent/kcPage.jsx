d24e157659a0119081f18eeb8c0167fb80ffab69
BagBags <aaronbagain@gmail.com>
2025-05-09 00:38:30 +0800

    What haffen  Vella?

--- JavaQuest/src/Components/userComponent/kcPage.jsx
+++ JavaQuest/src/Components/userComponent/kcPage.jsx
@@ -7,11 +7,8 @@ export default function KCPage({ KCQA, moduleID }) {
   const [incorrectAnswers, setIncorrectAnswers] = useState([]);
   const [score, setScore] = useState(null);
   const [correctAnswers, setCorrectAnswers] = useState([]);
-<<<<<<< HEAD
   const moduleQuestions = KCQA.filter((item) => item.moduleid === moduleID);
-=======
   const navigate = useNavigate();
->>>>>>> 97f7cfe3dc4db225e83e15089ec512f5d0cce2ca
 
   function KCCheckQA() {
     let calculatedScore = 0;
@@ -78,16 +75,11 @@ export default function KCPage({ KCQA, moduleID }) {
                 </span>
               </div>
               <div className="text-center mt-4 animate__animated animate__infinite flying-car">
-<<<<<<< HEAD
-                {score !== null && score / moduleQuestions.length >= 0.7 ? (
-                  <button className="gradient6 btn text-white ">
-=======
                 {score !== null && score / KCQA.length >= 0.7 ? (
                   <button
                     className="gradient6 btn text-white"
                     onClick={() => navigate("/quizInstructions")}
                   >
->>>>>>> 97f7cfe3dc4db225e83e15089ec512f5d0cce2ca
                     Take Quiz!
                   </button>
                 ) : (
