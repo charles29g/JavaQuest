04ffe784572b7c7eb944a5c918bcb474282e5f27
BagBags <aaronbagain@gmail.com>
2025-05-09 08:04:37 +0800

    Mongo DEbee

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const MONGO_URI = process.env.MONGO_URI;
+const punycode = require("punycode");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ backend/urls/URL.js
@@ -0,0 +1,4 @@
+const frontendURL = "http://localhost:5173";
+const backendURL = "http://localhost:5000";
+
+module.exports = { frontendURL, backendURL };
